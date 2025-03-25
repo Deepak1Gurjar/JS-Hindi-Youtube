dc6ea890f4bb0910244b9a16774572ff17e2ada7
Deepak1Gurjar <deepakchhalotre2002@gmail.com>
2025-03-25 09:41:21 +0530

    variables basic

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 144553
+let accountEmail = "deepak@google.com"
+var accountPassword = "12345"
+accountcity = "Indore"
+
+//accountId = 2 //not allowed
+accountEmail = "dg@dc.com"
+accountPassword = "212121"
+accountcity = "Harda"
+console.log(accountId);
+console.table([accountId, accountEmail, accountcity])
