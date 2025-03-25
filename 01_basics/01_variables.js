af79cb29c9bbd513f95f883e78925e3f06c77914
Deepak1Gurjar <deepakchhalotre2002@gmail.com>
2025-03-25 10:52:18 +0530

    datatypes

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,11 +1,15 @@
-const accountId = 144553
-let accountEmail = "deepak@google.com"
-var accountPassword = "12345"
-accountcity = "Indore"
+const accountId = 144553 // Cannot change the value of const 
+let accountEmail = "deepak@google.com" // Can change the value of let variable
+var accountPassword = "12345" // Can change the value of var variable
+accountcity = "Indore" //Without defining the datatype we can declare a variable in Java Script
+let accountstate; // It dosen't matter in JS that we use semicolon  
 
 //accountId = 2 //not allowed
 accountEmail = "dg@dc.com"
 accountPassword = "212121"
 accountcity = "Harda"
 console.log(accountId);
-console.table([accountId, accountEmail, accountcity])
+/*
+   Prefer not to use var because of issue in block scope and functional scope
+*/
+console.table([accountId, accountEmail, accountcity, accountstate])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; //treat all JS code as newer version
+
+//alert (3+3) //we are using nodejs not browser
+
+console.log(3+3);
+console.log("Deepak");
+
+let name="Deepak" // String type data
+let age=23        // Integer type data
+let isLoggedIn=false  // Boolean type data
+let state=null; // we intancenally said that it is not undefined but it is a null or empty value
+
+// PRIMITIVE DATA TYPES IN JS
+//number => 2 to Power 53
+//bigint
+//string => " "
+//boolean => true/false
+//null => standalone value
+//undefined =>
+//symbol =>unique
+
+//object
+
+console.log(typeof 'Deepak'); // typeof  it is used to identify the type of datatype 
+console.log(typeof age);
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
+
