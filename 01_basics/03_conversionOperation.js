89418fd50b402f38f7ea032690c715d424054a4a
Deepak1Gurjar <deepakchhalotre2002@gmail.com>
2025-03-25 12:44:26 +0530

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,24 @@
+let score = "Deepak"
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof (valueInNumber));
+console.log(valueInNumber); 
+
+// "33"=>33
+// "33abc"=> NaN (Not a Number)
+// true=>1 false=>=0
+
+// let isLoggedIn = "" //boolean value will we false // ""=>false
+// let isLoggedIn = "Deepak" //boolean value will we true // "Deepak" =>true
+let isLoggedIn = 1 //boolean value will we true  // 1=>true 0=>false
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+ 
+let someNumber = 33
+let stringNumber= String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
