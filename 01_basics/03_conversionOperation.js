0df58d10c32709f69deea3394eee9e5b83d80bca
Deepak1Gurjar <deepakchhalotre2002@gmail.com>
2025-03-27 14:06:09 +0530

    pre post increment

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -22,3 +22,42 @@ let stringNumber= String(someNumber);
 console.log(stringNumber);
 console.log(typeof stringNumber);
 
+// ************* Operations ***************** 
+
+let value=3;
+let negvalue= -value;
+console.log(negvalue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2/2);
+console.log(2%2);
+console.log(2**2);
+
+let str1="Deepak"
+let str2=" Gurjar"
+let str3= str1+str2;
+console.log(str3);  // Simple addition of two string
+
+console.log("1"+2);  // Addition of String and Number in this string is first then all numbers are treated as a String
+console.log(1+"2");  // Addition of Number and String
+console.log("1"+2+2); // Additon of String,Number and Number
+console.log(1+"2"+2); // Addition of Number,String and Number
+console.log(1+2+"2"); // Addition of Number,Number and String
+console.log((3+4)*5/3);
+console.log(+true);
+console.log(+"");
+let gamecounter=100;
+++gamecounter;
+console.log(gamecounter);
+
+let x=3;  //Post Increment
+const y=x++;
+console.log(y,x);
+
+let a=3; // Pre Increment
+const b=++a;
+console.log(b,a);
+
+
