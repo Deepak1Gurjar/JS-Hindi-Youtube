23ef0d82d2c2221ef77c78af871218e078f1c162
Deepak1Gurjar <deepakchhalotre2002@gmail.com>
2025-03-27 14:07:01 +0530

    comparirsion of Operators

---
+++ 01_basics/04_compairsion.js
@@ -0,0 +1,20 @@
+console.log(2>1);
+console.log(2>=1);
+console.log(2<1);
+console.log(2<=1);
+console.log(2==1); // when we compair to values togther we must asure that they are of same data type
+
+console.log("2">1);
+console.log("02">1);
+
+/*console.log(null > 0);   // These type of conversion are get you into confusion
+console.log(null == 0);
+console.log(null >=0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+*/
+
+//===(triple equal to) //strict check => check values along with datatypes
+console.log("2"===2);
