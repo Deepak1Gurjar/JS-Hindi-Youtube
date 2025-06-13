a0f88fa84473bfa251a5616a2da326bd560460e1
Deepak1Gurjar <deepakchhalotre2002@gmail.com>
2025-06-13 14:21:31 +0530

    equality and compairsion operator work differently

--- 01_basics/04_compairsion.js
+++ 01_basics/04_compairsion.js
@@ -8,10 +8,10 @@ console.log("2">1);
 console.log("02">1);
 
 /*console.log(null > 0);   // These type of conversion are get you into confusion
-console.log(null == 0);
-console.log(null >=0);
+console.log(null == 0); //The reason is that equality and compairsion operator work differently 
+console.log(null >=0);  // Compairsion convert null to a number=0 that why 0>0 is false and 0>=0 is true
 
-console.log(undefined == 0);
+console.log(undefined == 0); // In these 3 compairsion all output will be false
 console.log(undefined > 0);
 console.log(undefined < 0);
 */
